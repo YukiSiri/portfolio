3f3ed604cd02c663b835d2d34d418b557f6a1ce8
alexandrelevenez <alexandre.levenez@ecoles-epsi.net>
2025-04-29 17:30:07 +0200

    Modification des liens de téléchargement dans la section des rapports pour utiliser des chemins absolus au lieu de relatifs. Remplacement des composants Link par des balises <a> pour une meilleure accessibilité.

--- components/reports-section.tsx
+++ components/reports-section.tsx
@@ -1,5 +1,4 @@
 // components/reports-section.tsx
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 export default function ReportsSection() {
@@ -8,14 +7,14 @@ export default function ReportsSection() {
       title: "Rapport de stage - Entreprise La tête dans la toile",
       description: "Stage de développement web - 5 semaines",
       date: "Avril - Mai 2024",
-      fileUrl: "../documents/Rapport-de-stage-LEVENEZ-Alexandre-1.pdf",
+      fileUrl: "/documents/Rapport-de-stage-LEVENEZ-Alexandre-1.pdf",
       fileName: "Rapport-de-stage-LEVENEZ-Alexandre-1.pdf", // Nom du fichier à télécharger
     },
     {
       title: "Rapport de stage - Entreprise PLR Conseil",
       description: "Stage de développement - 5 semaines",
       date: "Janvier - Février 2025",
-      fileUrl: "../documents/Rapport-de-stage-LEVENEZ-Alexandre-2.pdf",
+      fileUrl: "/documents/Rapport-de-stage-LEVENEZ-Alexandre-2.pdf",
       fileName: "Rapport-de-stage-LEVENEZ-Alexandre-2.pdf", // Nom du fichier à télécharger
     },
   ];
@@ -32,11 +31,16 @@ export default function ReportsSection() {
 
         <div className="flex flex-col items-center">
           {reports.map((report, index) => (
-            <Link key={index} href={report.fileUrl} download={report.fileName}>
-              <Button variant="outline" className="mb-4">
-                {report.title} - {report.date}
-              </Button>
-            </Link>
+            <a
+            key={index}
+            href={report.fileUrl}
+            download={report.fileName}
+            className="mb-4"
+          >
+            <Button variant="outline">
+              {report.title} - {report.date}
+            </Button>
+          </a>
           ))}
         </div>
       </div>
